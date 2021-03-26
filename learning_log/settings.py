e10292634d745966290d79e589b684482d689a41
Dohl9k <ze22ro@gmail.com>
2021-03-26 15:57:28 +0300

    initial commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'qlrvfq_g5!v-t=9nkguulz@-t&7ojq7kmsx8vdvxit#72u$jmu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
